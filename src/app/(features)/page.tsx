ef4ef6f0483cff5f7745c43b2af34985132f23c9
KeiIT11 <keiit596@gmail.com>
2023-09-14 00:24:01 +0900

    ナビゲーションバーを追加しました

---
+++ src/app/(features)/page.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import Homenavbar from '../_components/Header/navbar'
+import Head from 'next/head'
+//import Link from 'next/link';
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>test</title>
+      </Head>
+      <body>
+        <header>
+          <Homenavbar />
+        </header>
+        <main>
+          <div>
+            <p>content</p>
+          </div>
+        </main>
+      </body>
+    </>
+  )
+}
